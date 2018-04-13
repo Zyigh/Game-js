8751af74796ba57c2d1408caa89f3a25e7978c87
Hugo Medina <hmedina.paris75@gmail.com>
2018-04-13 20:01:27 +0200

    YOOOOOOOLOOOOOO

---
+++ app/Data.js
@@ -0,0 +1,21 @@
+class Data {
+    getQuestions() {
+        return [
+            {
+                description: "Les multiplications, c'est cool",
+                question: "var a = 8;\nvar b = 12;\nCombien font 6 * 7 ?",
+                answer: 42,
+                clues: ["Tu l'as vu en primaire...",
+                    "C'est dans H2G2...",
+                    "La réponse à la grande question de la vie de l'univers et de tout ce qui est..."]
+            },{
+                description: "Additionner, c'est comme ajouter, mais en plus classe",
+                question: "J'achète un cheval. Combien font 40 + 2 ?",
+                answer: 42,
+                clues: ["Tu l'as vu en primaire...",
+                    "C'est dans H2G2...",
+                    "La réponse à la grande question de la vie de l'univers et de tout ce qui est..."]
+            }
+        ];
+    }
+}
